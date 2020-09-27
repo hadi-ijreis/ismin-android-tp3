a9faea711310f50c54586eae353a058d0bab3d03
Gaëtan Maisse <gmaisse@solocal.com>
2020-09-27 12:00:48 +0200

    chore: init project

---
+++ app/src/main/java/com/ismin/androidtp2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ismin.androidtp2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
